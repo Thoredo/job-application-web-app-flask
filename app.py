c3a6f39284d416fae6ee1ee304f7addc3af1a9e4
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-08-16 15:10:02 +0200

    Add confirmation email function

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 from dotenv import load_dotenv
 
 
@@ -11,8 +12,15 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MY_EMAIL")
+app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASSWORD")
 
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -43,6 +51,21 @@ def index():
         )
         db.session.add(form)
         db.session.commit()
+
+        message_body = (
+            f"Thank you for your submission, {first_name}. "
+            f"Here is your data:\n{first_name}\n{last_name}\n{date}\n"
+            f"Thank you!"
+        )
+
+        message = Message(
+            subject="New form submission",
+            sender=app.config["MAIL_USERNAME"],
+            recipients=[email],
+            body=message_body,
+        )
+        mail.send(message)
+
         flash(f"{first_name}, your form was submitted succesfully!", "succes")
 
     return render_template("index.html")
